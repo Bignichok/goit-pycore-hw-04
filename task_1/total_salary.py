7876193d4978a7f6650ede375cebb048d8f6f693
bignichok <inikita546@gmail.com>
2024-04-11 17:56:14 +0300

    rask 1

--- task_1/total_salary.py
+++ task_1/total_salary.py
@@ -1,18 +1,14 @@
-import pathlib
-
-current_dir = pathlib.Path(__file__).parent
-
-def total_salary(filename: str) -> tuple[int, int]:
+def total_salary(path: str) -> tuple[int, int]:
     try:
-        with open(current_dir / filename, "r") as file:
+        with open(path, "r", encoding="utf-8") as file:
             salaries = [int(line.strip().split(",")[1]) for line in file]
             total_sum = sum(salaries)
             average_salary = total_sum // len(salaries)
             return total_sum, average_salary
     except FileNotFoundError:
-        print("file is not exists") 
+        print("file does not exist") 
         return 0, 0
 
 
-total, average = total_salary('salaries.txt')
+total, average = total_salary('task_1\salaries.txt')
 print(f"Total sum of salaries: {total}, Average salary: {average}")
